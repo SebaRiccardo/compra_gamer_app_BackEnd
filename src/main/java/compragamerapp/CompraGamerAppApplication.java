dc631a30c1028a6348afbeebb8cad858ecdd6234
soathekid <sebaoaki@gmail.com>
2020-07-05 02:10:22 -0300

    nada

--- src/main/java/compragamerapp/CompraGamerAppApplication.java
+++ src/main/java/compragamerapp/CompraGamerAppApplication.java
@@ -2,6 +2,7 @@ package compragamerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CompraGamerAppApplication {
@@ -9,5 +10,4 @@ public class CompraGamerAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CompraGamerAppApplication.class, args);
 	}
-
 }
