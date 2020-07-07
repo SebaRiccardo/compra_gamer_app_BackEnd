e31a7c3c6d3b53426800df8fa4f729892139d31b
soathekid <sebaoaki@gmail.com>
2020-07-07 09:06:20 -0300

    limpieza

--- src/main/java/compragamerapp/services/PurchaseService.java
+++ src/main/java/compragamerapp/services/PurchaseService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import compragamerapp.entities.Purchase;
 import compragamerapp.repository.PurchaseRepository;
 
@@ -13,18 +12,22 @@ import compragamerapp.repository.PurchaseRepository;
 public class PurchaseService {
     @Autowired
     PurchaseRepository purchaseRepository;
+    
 
     public List<Purchase> getAll(){
         return purchaseRepository.findAll();
     } 
     public  Purchase getPurchase(Long id){
+        
         return  purchaseRepository.findById(id).orElse(null);
+    
     }
-    public Purchase savePurchase(Purchase purchase){
-        return purchaseRepository.save(purchase);
+    public Purchase savePurchase(Purchase p){  
+       return purchaseRepository.save(p);
     }
     public void deletePurchase(Long id){
         purchaseRepository.deleteById(id);
     }
+
 
 }
